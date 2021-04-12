1e3a6300828a74630af24f19e515ec9296038eeb
Simon Angel <simonang@bu.edu>
2021-04-12 11:29:18 -0400

    Just updating my branch

--- app/src/main/java/com/company/vesper/HomeFragment.java
+++ app/src/main/java/com/company/vesper/HomeFragment.java
@@ -4,10 +4,24 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.company.vesper.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +35,10 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    // Initialize watchlist globally in this file
+    List<String> watchlist = new ArrayList<String>() ;
+    // Declare listView
+    ListView myStockList;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -62,7 +80,67 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        FirebaseFirestore snapshot_db ;
+        CollectionReference snapshot_collection;
+
         // Inflate the layout for this fragment
+        // State ?
+        // getDatabase() : entry point for all databases
+        // .collection : used for specifying collection path
+        //
+        // get : Reads the document referred to by this
+
+        // CHECK : Successfully getting Uid
+        //String userId = (String) State.getUser().getUid() ;
+        //QuerySnapshot snapshot = State.getDatabase().collection("user").whereEqualTo("email", "ericjyc@bu.edu").get().getResult();
+
+        // CHECK : Seems to work fine, does not cause runtime crash
+        // CHECK: No runtime crash yet ()
+//        Task<DocumentSnapshot> snapshot_document = snapshot_collection.document(userId).get();
+//        // THE LAST .getResult() is the problem
+//        DocumentSnapshot get_object = snapshot_document.getResult();
+
+        // Put the data into a local data structure
+        //watchlist = (List<String>) snapshot.get("watchlist");
+
+        // NEXT GOAL : Create listview according to diagram 10 in the UI
+
+        State.getDatabase().collection("users").document(State.getUser().getUid()).get().addOnCompleteListener(t -> {
+            DocumentSnapshot snapshot = t.getResult();
+            List<String> watchlist = (List<String>) snapshot.get("watchlist");
+        });
+        //Log.i(watchlist);
+        // WATCHLIST HAS TICKERS
+        // LETS MAKE SOME NICE LISTVIEWS
+
+
+
+        //Build array adapter object to with listview options
+       # ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                #android.R.layout.z, watchlist);
+
+        myStockList = (ListView) findViewById(R.id.);
+        // Build the listview with the options in the array
+        myStockList.setAdapter(adapter);
+        // Set an handler to catch which option the user chooses in case we want it to do something
+
+
+//        myStockList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//
+//                choice = (String) myStockList.getItemAtPosition(position);
+//
+//                if( choice == "Easy" ){
+//                    MODE = 1;
+//                } else if(choice == "Medium" ) {
+//                    MODE = 2;
+//                } else if (choice == "Hard"){
+//                    MODE = 3;
+//                }
+//            }
+//        });
         return binding.getRoot();
     }
 }
