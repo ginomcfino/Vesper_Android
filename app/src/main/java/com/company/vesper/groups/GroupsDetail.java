e57e7cd41298350f980f75982d46563fef81aa75
Weiqi Ji <44929402+ginomcfino@users.noreply.github.com>
2021-04-19 15:33:28 -0400

    working signals count

--- app/src/main/java/com/company/vesper/groups/GroupsDetail.java
+++ app/src/main/java/com/company/vesper/groups/GroupsDetail.java
@@ -9,8 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.company.vesper.R;
+import com.company.vesper.State;
 import com.company.vesper.databinding.FragmentGroupsDetailBinding;
 import com.company.vesper.dbModels.GroupInfo;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,6 +47,18 @@ public class GroupsDetail extends Fragment {
 
         binding.excellentCount.setText(String.valueOf(group.getExcellent_signals()));
         binding.goodCount.setText(String.valueOf(group.getGood_signals()));
+
+
+        State.getDatabase().collection("signals")
+                .whereIn("group", Arrays.asList(new DocumentReference[] {group.getRef()}))
+                .get()
+                .addOnCompleteListener(task -> {
+                    QuerySnapshot qSnap = task.getResult();
+                    int signalCount = qSnap.getDocuments().size();
+                    binding.signalsCount.setText(String.valueOf(signalCount));
+                });
+
+
 
 
         // Inflate the layout for this fragment
