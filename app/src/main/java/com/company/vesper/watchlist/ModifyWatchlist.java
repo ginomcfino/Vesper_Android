61c29255c318f08cbf55ab4e6efc50e53d291fc9
Simon Angel <simonang@bu.edu>
2021-04-19 13:06:49 -0400

    Removed Hardcoded strings and localized them

--- app/src/main/java/com/company/vesper/watchlist/ModifyWatchlist.java
+++ app/src/main/java/com/company/vesper/watchlist/ModifyWatchlist.java
@@ -69,7 +69,7 @@ public class ModifyWatchlist extends Fragment {
             public void onClick(View view) {
                 flag = addFlag;
                 // Toast you are adding a stock
-                Toast.makeText(getActivity(), "Ready to ADD", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), getString(R.string.ready_add), Toast.LENGTH_SHORT).show();
             }
 
         });
@@ -77,7 +77,7 @@ public class ModifyWatchlist extends Fragment {
             @Override
             public void onClick(View view) {
                 flag = deleteFlag;
-                Toast.makeText(getActivity(), "Ready to DELETE", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), getString(R.string.ready_delete), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -85,7 +85,7 @@ public class ModifyWatchlist extends Fragment {
         SearchView simpleSearchView = view.findViewById(R.id.searchBar);
         simpleSearchView.setSubmitButtonEnabled(true);
         // Tell user what to do
-        simpleSearchView.setQueryHint("Select Add or Remove");
+        simpleSearchView.setQueryHint("Select a Button Below before Submitting");
         // perform set on query text listener
         simpleSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -93,7 +93,7 @@ public class ModifyWatchlist extends Fragment {
                 try {
                     // Check the flag
                     if (flag == nullValue){
-                        Toast.makeText(getActivity(), "Select Add or Remove", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), getString(R.string.select_add_remove), Toast.LENGTH_SHORT).show();
                     } else if (flag == deleteFlag){
 
                         for (WatchListItem currentItem : watchlist_array) {
@@ -106,23 +106,10 @@ public class ModifyWatchlist extends Fragment {
                                 UserInfo.removeFromWatchlist(Ticker);
                             }
                         }
-
-//                        Toast.makeText(getActivity(), "Deleted Ticker", Toast.LENGTH_SHORT).show();
-//                        // Check that Ticker is in proper format
-//                        if (checkTicker(Ticker)) {
-//                            //deleteStock(Ticker, adapter);
-//                        }else{
-//                            Toast.makeText(getActivity(), "Enter a Ticker", Toast.LENGTH_SHORT).show();
-//                            Toast.makeText(getActivity(), "Capital Letters Only ", Toast.LENGTH_SHORT).show();
-//                        }
-
                     } else{
                         // Try to add the ticker to the watchlist
-                        // Put this in a method.
                         Toast.makeText(getActivity(), getString(R.string.added_ticker_toast), Toast.LENGTH_SHORT).show();
                         // Pass the ticker to add and the adapter
-                        //processIntoWatchlist(Ticker, adapter);
-
                         // add it to watchlist_array
                         WatchListItem watchListItem = new WatchListItem(Ticker);
                         // Now we wait for the real data
@@ -134,13 +121,10 @@ public class ModifyWatchlist extends Fragment {
                             adapter.notifyDataSetChanged();
                             UserInfo.addToWatchlist(Ticker);
                         });
-
-                        // Check that its all caps lock
                     }
-
                 }
                 catch(Exception e) {
-                    System.out.println("Not able to add to watchlist, wait and try again");
+                    System.out.println(getString(R.string.add_error));
                 }
                 return false;
             }
@@ -165,8 +149,6 @@ public class ModifyWatchlist extends Fragment {
 
         // Loop over every ticker symbol, for each one create a watchListItem and add it to the array
         for (int i = 0; i < tickerSymbols.size(); i++) {
-            //processIntoWatchlist(tickerSymbols.get(i), adapter);
-
             // add it to watchlist_array
             WatchListItem watchListItem = new WatchListItem(tickerSymbols.get(i));
             watchlist_array.add(watchListItem);
@@ -181,18 +163,6 @@ public class ModifyWatchlist extends Fragment {
         }
         return view;
     }
-
-//    private WatchListItem findWatchlistItem(String ticker, List<WatchListItem>  watchlist_array) {
-//
-//        for (WatchListItem currentItem : watchlist_array) {
-//            if (currentItem.Ticker == ticker) {
-//                return currentItem;
-//            }
-//        }
-//        return 0;
-//    }
-
-
 
 //    private static boolean checkTicker(String Ticker) {
 //        char ch;
