7884b2be2444a442f6faedd57acfe163ea6af186
Weiqi Ji <44929402+ginomcfino@users.noreply.github.com>
2021-04-19 21:53:24 -0400

    finished personal settings

--- app/src/main/java/com/company/vesper/UserProfile.java
+++ app/src/main/java/com/company/vesper/UserProfile.java
@@ -3,10 +3,26 @@ package com.company.vesper;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import com.company.vesper.State;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import com.company.vesper.databinding.FragmentModifyWatchlistBinding;
+import com.company.vesper.databinding.FragmentUserProfileBinding;
+import com.company.vesper.dbModels.GroupInfo;
+import com.company.vesper.dbModels.UserInfo;
+import com.company.vesper.groups.GroupListAdapter;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +30,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class UserProfile extends Fragment {
+
+    ExpandableListView expandableListView;
+    TextView username;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,8 +51,6 @@ public class UserProfile extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment UserProfile.
      */
     // TODO: Rename and change types and number of parameters
@@ -59,6 +76,25 @@ public class UserProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        //return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        ;
+        FragmentUserProfileBinding binding = FragmentUserProfileBinding.inflate(inflater);
+        View view = binding.getRoot();
+
+        UserInfo user = State.getUser();
+
+        username = view.findViewById(R.id.username);
+        username.setText(user.getName());
+
+        List<GroupInfo> groups = new ArrayList<>();
+        GroupListAdapter adapter = new GroupListAdapter(getContext(), groups);
+
+        groups.addAll(State.getUser().getGroups());
+
+        adapter.notifyDataSetChanged();
+
+        binding.joinedGroups.setAdapter(adapter);
+
+        return view;
     }
 }
